5ecd3e637252329b43e44c512a8a6902e7995e15
tahsin <u1704102@student.cuet.ac.bd>
2024-10-08 02:43:30 +0600

    done so fart

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+require('dotenv').config({ path:'/.env' });
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({ extended: true }));
+
+
+
+const indexRoutes = require('./routes/index');
+app.use('/', indexRoutes);
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Frontend running at ${port}`);
+});
